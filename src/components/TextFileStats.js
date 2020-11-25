f6354d8f2954780ebcfb4747d38da4bf5cf5eb05
Ian Boucher <ianboucher@Ians-MacBook-Pro.local>
2020-11-25 19:16:47 +0000

    Style Stats in Grid

--- src/components/TextFileStats.js
+++ src/components/TextFileStats.js
@@ -8,52 +8,53 @@ import {
     getMostCommonLetter,
     getWordCount
 } from '../statistics';
+import '../TextFileStats.css'
 
-export default function StatsGroup(props) {
+export default function TextFileStats(props) {
 
     const { files, id } = props;
     const [text, setText] = useState('');
     useEffect(() => readFile(files, setText));
 
     return (
-        <div id={id} data-testid={id} className="StatsGroup-grid">
+        <div id={id} data-testid={id} className="TestFileStats-grid">
 
-            <div id="word-count" data-testid="word-count" className="StatsGroup-grid-item">
+            <div id="word-count" data-testid="word-count" className="TestFileStats-grid-item">
                 <Stat
                     statName={"Total Word Count"}
                     statValue={getWordCount(text)}
                 />
             </div>
 
-            <div id="line-count" data-testid="line-count" className="StatsGroup-grid-item">
+            <div id="line-count" data-testid="line-count" className="TestFileStats-grid-item">
                 <Stat
                     statName={"Total Line Count"}
                     statValue={getLineCount(text)}
                 />
             </div>
 
-            <div id="mean-word-length" data-testid="mean-word-length" className="StatsGroup-grid-item">
+            <div id="mean-word-length" data-testid="mean-word-length" className="TestFileStats-grid-item">
                 <Stat
                     statName={"Mean Word Length"}
                     statValue={getMeanWordLength(text)}
                 />
             </div>
 
-            <div id="modal-word-length" data-testid="modal-word-length" className="StatsGroup-grid-item">
+            <div id="modal-word-length" data-testid="modal-word-length" className="TestFileStats-grid-item">
                 <Stat
                     statName={"Modal Word Length"}
                     statValue={getModalWordLength(text)}
                 />
             </div>
 
-            <div id="median-word-length" data-testid="median-word-length" className="StatsGroup-grid-item">
+            <div id="median-word-length" data-testid="median-word-length" className="TestFileStats-grid-item">
                 <Stat
                     statName={"Median Word Length"}
                     statValue={getMedianWordLength(text)}
                 />
             </div>
 
-            <div id="most-common-letter" data-testid="most-common-letter" className="StatsGroup-grid-item">
+            <div id="most-common-letter" data-testid="most-common-letter" className="TestFileStats-grid-item">
                 <Stat
                     statName={"Most Common Letter"}
                     statValue={getMostCommonLetter(text)}
